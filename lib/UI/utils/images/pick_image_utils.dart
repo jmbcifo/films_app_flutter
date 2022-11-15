d17fd2699db2cd90e4344363021fea3682abc2dd
jmbscifo <josemanuelbarbacifo@gmail.com>
2022-11-15 11:45:15 +0100

    get url

--- lib/UI/utils/images/pick_image_utils.dart
+++ lib/UI/utils/images/pick_image_utils.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+import 'package:films_app_flutter/models/user_model.dart';
+import 'package:films_app_flutter/structure/controllers/auth_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class PickImageUtils {
   void showPicker(BuildContext context) {
@@ -30,14 +34,31 @@ class PickImageUtils {
   Future imgFromGallery() async {
     final picker = ImagePicker();
     XFile? image = await picker.pickImage(
-      source: ImageSource.gallery,
+      source: ImageSource.camera,
       imageQuality: 50,
     );
 
     if (image != null) {
       print(image.path);
     }
+    uploadFile(File(image!.path));
   }
 
-  Future uploadFile() async {}
+  Future uploadFile(File file) async {
+    //Crear la referencia en firebase
+    firebase_storage.Reference storageReference = firebase_storage
+        .FirebaseStorage.instance
+        .ref()
+        .child('imagesProfile/123456');
+
+    //Transformar el path local en url
+    firebase_storage.UploadTask uploadTask = storageReference.putFile(file);
+
+    await uploadTask.whenComplete(() {
+      //Obtener el url de Firebase
+      storageReference.getDownloadURL().then((url) {
+        print(url);
+      });
+    });
+  }
 }
